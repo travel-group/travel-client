e060dda245e5950a77b1bc42f9dc711b41ad8f98
mohndkh20 <mondkaled4@gmail.com>
2022-07-04 10:24:38 +0300

    edit account

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -9,49 +9,21 @@ const Account = () => {
   const auth = useContext(AuthContext)
   const navigate = useNavigate()
 
-<<<<<<< HEAD
-  console.log(auth, '------------')
-  // useEffect(() => {
+  useEffect(() => {
   if (!auth.isAuthenticated) {
     navigate('/login')
-    console.log('---no auth---------')
-
   }
-  //  }, [])
+    }, [])
   if (auth?.user?.admin == false) {
-
     console.log('--ppppppppppppp---------')
-
   }
 
-
-  return (
-    <div className="my-5">
-      asdfghjk
-    </div>
-  )
-=======
-    // console.log(auth,'------------')
-    useEffect(() => {
-        if (!auth.isAuthenticated) {
-          navigate('/login')
-          // console.log('---no auth---------')
-
-        }
-      }, [])
-        // if (auth?.user?.admin==false) {
-          
-          //   console.log('--ppppppppppppp---------')
-          
-          // }
-          
           
           return (
             <div className="my-5">
             {(auth?.user?.admin == false) && (<User />)}
         </div>
     )
->>>>>>> 601b70885a970a89c5c2f67d33429f458b37a688
 }
 
 export default Account
