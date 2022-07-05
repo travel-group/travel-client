039a754dc13df8c9aec712be49ed7ca38d90f91a
mohndkh20 <mondkaled4@gmail.com>
2022-07-05 11:05:58 +0300

    fixed logout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Footer from "./components/Footer";
 import SinglePost from "./components/SinglePost";
 import SignUp from "./components/account/SignUp";
 import Login from "./components/account/Login";
-import LogOut from "./components/account/Logout";
 import Nav from "./components/Nav";
-import Post from "./components/posts/Post";
 import AllPosts from "./components/posts/AllPosts";
 import Countries from "./components/posts/Summer";
 import Account from "./components/account/Account";
@@ -14,8 +12,6 @@ import Posts from "././components/account/user/Posts";
 import AddPost from "./components/account/user/AddPost";
 import EditProfile from "./components/account/user/EditProfile";
 import EditPost from "./components/account/user/EditPost";
-
-import Account2 from "./components/posts/Winter";
 import Summer from "./components/posts/Summer";
 import Winter from "./components/posts/Winter";
 
@@ -29,7 +25,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<LogOut />} />
         <Route path="/" element={<Home />} />
         <Route path="/singlepost" element={<SinglePost />} />
         {/* <Route path="/account" element={<Account />} /> */}
